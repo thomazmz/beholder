831c06497a5b33e22d19f3d26dee0b9fb3684af2
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2021-01-16 19:27:46 -0300

    Include missing exports into index file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
+export { useStringValidators } from './useStringValidators'
 export { useDictionary } from './useDictionary/useDictionary'
 export { usePrevious } from './usePrevious'
+export { useInput } from './useInput'
